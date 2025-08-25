2df67183f73263c16200b2e6435c2f0fc72c73e1
Peter Sanderson <dierk.deidel@protonmail.com>
2025-08-25 11:16:27 +0200

    feat: add basic code to run relay server together with payment-gate

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
+import {createConsole} from "@evolu/common";
+import {createNodeJsRelay} from "@evolu/nodejs";
+import {mkdirSync} from "fs";
 import fastify from 'fastify'
+
+// ============================================
+// =============== EVOLU RELAY ================
+
+const deps = {
+    console: createConsole(),
+};
+
+// Ensure the database is created in a predictable location for Docker.
+mkdirSync("data", {recursive: true});
+process.chdir("data");
+
+const relay = await createNodeJsRelay(deps)({
+    port: 4000,
+    enableLogging: false,
+});
+
+process.on("SIGINT", relay[Symbol.dispose]);
+process.on("SIGTERM", relay[Symbol.dispose]);
+
+
+// ============================================
+// =============== PAYMENT GATE ===============
 
 const server = fastify()
 
