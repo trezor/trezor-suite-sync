36eeabf009e50ae16ad6b5c8a6514ed6fc92164c
Matěj Husák <matej.husak@icloud.com>
2025-11-10 15:46:59 +0100

    feat: add health endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { mkdirSync } from 'fs';
 import { join } from 'path';
 
 import { startEvoluRelay } from './evoluRelay/relay.js';
+import { startHealthServer } from './health/startHealthServer.js';
 import { startQuotaManagerServer } from './quoteManager/server.js';
 import { createAppStorage } from './storage.js';
 
@@ -10,23 +11,40 @@ const RELAY_PORT = process.env.RELAY_PORT ? parseInt(process.env.RELAY_PORT, 10)
 const QUOTA_MANAGER_PORT = process.env.QUOTA_MANAGER_PORT
     ? parseInt(process.env.QUOTA_MANAGER_PORT, 10)
     : 4001;
+const HEALTH_SERVER_PORT = process.env.HEALTH_PORT ? parseInt(process.env.HEALTH_PORT, 10) : 4002;
+
 const DATA_DIR = process.env.DATA_DIR || 'data';
 
 const dataPath = join(process.cwd(), DATA_DIR);
 mkdirSync(dataPath, { recursive: true });
 process.chdir(dataPath);
 
+const updateHealth = startHealthServer({
+    port: HEALTH_SERVER_PORT,
+});
+
 const storage = await createAppStorage();
 
 if (storage.ok) {
     const { limitStorage, challengeStorage } = storage.value;
 
-    startEvoluRelay({ port: RELAY_PORT, limitStorage });
-    startQuotaManagerServer({
+    const evoluStarted = await startEvoluRelay({
+        port: RELAY_PORT,
+        limitStorage,
+        onHealthChange: updateHealth,
+    });
+    const quotaManagerStarted = await startQuotaManagerServer({
         port: QUOTA_MANAGER_PORT,
         limitStorage,
         challengeStorage,
+        onHealthChange: updateHealth,
     });
+
+    if (!evoluStarted && !quotaManagerStarted) {
+        // eslint-disable-next-line no-console
+        console.log('Evolu Relay and Quota Manager started failed, exiting...');
+        process.exit(1);
+    }
 } else {
     console.error('Cannot start server, error: ', storage.error);
 }
--- src/quoteManager/server.ts
+++ src/quoteManager/server.ts
@@ -2,9 +2,11 @@ import { randomBytes } from 'crypto';
 import fastify from 'fastify';
 
 import { registerChallengeEndpoints } from './challenge/registerChallengeEndpoints.js';
+import { createCustomErrorHandler } from './createCustomErrorHandler.js';
 import { evoluValidatorCompiler } from './evoluValidatorCompiler.js';
 import { registerStorageEndpoints } from './storage/registerStorageEndpoints.js';
 import { registerSyncEndpoints } from './sync/registerSyncEndpoints.js';
+import { UpdateHealth } from '../health/startHealthServer.js';
 import type { ChallengeStorage } from '../storage/challengeStorage/challengeStorage.js';
 import type { LimitStorage } from '../storage/limitStorage/limitStorage.js';
 
@@ -14,37 +16,48 @@ type StartQuotaManagerServerDependencies = {
     port: number;
     limitStorage: LimitStorage;
     challengeStorage: ChallengeStorage;
+    onHealthChange: UpdateHealth;
 };
 
-export const startQuotaManagerServer = ({
+export const startQuotaManagerServer = async ({
     port,
     limitStorage,
     challengeStorage,
+    onHealthChange,
 }: StartQuotaManagerServerDependencies) => {
     const server = fastify();
 
-    server.setValidatorCompiler(evoluValidatorCompiler);
+    try {
+        server.setValidatorCompiler(evoluValidatorCompiler);
 
-    registerStorageEndpoints({ server, limitStorage });
-    registerSyncEndpoints({ server });
-    registerChallengeEndpoints({ server, challengeStorage, createRandomBytes });
+        server.setErrorHandler(createCustomErrorHandler(onHealthChange));
 
-    server.listen({ port, host: '0.0.0.0' }, (err, address) => {
-        if (err) {
-            console.error(err);
-            process.exit(1);
-        }
+        registerStorageEndpoints({ server, limitStorage });
+        registerSyncEndpoints({ server });
+        registerChallengeEndpoints({ server, challengeStorage, createRandomBytes });
 
+        const address = await server.listen({ port, host: '0.0.0.0' });
+
+        onHealthChange({ quotaManager: 'ok' });
         // eslint-disable-next-line no-console
         console.log(`Payment Server (Quota Manager) started on ${address}`);
-    });
+    } catch (err) {
+        console.error('Failed to start Payment Server (Quota Manager):', err);
+
+        onHealthChange({ quotaManager: 'error' });
+
+        return false;
+    }
 
     const close = () => {
         // eslint-disable-next-line no-console
         console.log('Payment Server (Quota Manager) is shutting down...');
+        onHealthChange({ quotaManager: 'exiting' });
         server.close();
     };
 
     process.on('SIGINT', close);
     process.on('SIGTERM', close);
+
+    return true;
 };
