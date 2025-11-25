69b74e1879a02f56ead612a0a2dc07e414c4d717
Matěj Husák <matej.husak@icloud.com>
2025-11-25 21:42:20 +0100

    fix: dynamic import for device authenticity package

--- src/quoteManager/storage/endpoints/register/storageRegisterHandler.test.ts
+++ src/quoteManager/storage/endpoints/register/storageRegisterHandler.test.ts
@@ -19,31 +19,36 @@ import {
 import { prepareSqlite } from '../../../../storage/prepareSqlite.js';
 import { evoluValidatorCompiler } from '../../../evoluValidatorCompiler.js';
 
-vi.mock('@trezor/device-authenticity', () => ({
+const { T2B1rootPubKeyOptiga } = vi.hoisted(() => ({
+    T2B1rootPubKeyOptiga:
+        '04626d58aca84f0fcb52ea63f0eb08de1067b8d406574a715d5e7928f4b67f113a00fb5c5918e74d2327311946c446b242c20fe7347482999bdc1e229b94e27d96',
+    mockParseCertificate: vi.fn(),
+}));
+
+vi.mock('../../utils/deviceAuthenticationWrapper.ts', () => ({
     verifyAuthenticityProof: vi.fn().mockResolvedValue({
         valid: true,
         caPubKey: 'test-ca-pubkey',
-        rootPubKey: 'test-root-pubkey',
+        rootPubKey: T2B1rootPubKeyOptiga,
     }),
-    parseCertificate: vi.fn().mockReturnValue({
-        tbsCertificate: {
-            subjectPublicKeyInfo: {
-                algorithm: {
-                    algorithmName: 'P-256',
-                },
-                bits: {
-                    bytes: Buffer.from(
-                        '049bbf06dad9ab5905e05471ce16d5222c89c2caa39f26267ac0747129885fbd441bcc7fa84de120a36755daf30a6f47e8c0d4bddc15036ed2a3447dfa7a1d3e88',
-                        'hex',
-                    ),
-                },
-            },
+    getDeviceAuthenticityBlacklistConfig: vi.fn().mockResolvedValue({
+        version: 1,
+        blacklistedCaPubKeys: [],
+        debug: {
+            blacklistedCaPubKeys: [],
         },
     }),
-    deviceAuthenticityConfig: {},
-    deviceAuthenticityBlacklistConfig: {},
+    getDeviceAuthenticityConfig: vi.fn().mockResolvedValue({
+        version: 1,
+        T2B1: { rootPubKeysOptiga: [T2B1rootPubKeyOptiga] },
+        T3B1: { rootPubKeysOptiga: [] },
+        T3T1: { rootPubKeysOptiga: [] },
+        T3W1: {
+            rootPubKeysOptiga: [],
+            rootPubKeysTropic: ['cd318dc8405ae4f4144e3284dcb7b0cb0f0c2195c2ca14a0f6fccd9104e32a4b'],
+        },
+    }),
 }));
-
 const publicKey = getOrThrowTest(PublicKey.from('test-pubkey-123'));
 const size100 = getOrThrowTest(Size.from(100));
 
