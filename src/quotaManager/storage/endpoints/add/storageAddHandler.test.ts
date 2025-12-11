d6bfe9fb1a20b45dada174cb2ea4688357945f2d
Matěj Husák <matej.husak@icloud.com>
2025-12-11 12:45:50 +0100

    chore(quota): removed dynamic wrapper as it is ES module native now

--- src/quotaManager/storage/endpoints/add/storageAddHandler.test.ts
+++ src/quotaManager/storage/endpoints/add/storageAddHandler.test.ts
@@ -1,4 +1,5 @@
 import { OwnerId } from '@evolu/common';
+import { verifyAuthenticityProof } from '@trezor/device-authenticity';
 import Fastify from 'fastify';
 import { assert, beforeEach, describe, expect, it, vi } from 'vitest';
 
@@ -19,27 +20,26 @@ import {
 import { addLimitToPubkey } from '../../../../storage/limitStorage/methods/addLimitToPubkey.js';
 import { prepareTestDatabase } from '../../../../storage/limitStorage/prepareTestDatabase.js';
 import { evoluValidatorCompiler } from '../../../evoluValidatorCompiler.js';
-import { verifyAuthenticityProof } from '../../utils/deviceAuthenticationWrapper.js';
 
 const { T2B1rootPubKeyOptiga } = vi.hoisted(() => ({
     T2B1rootPubKeyOptiga:
         '04626d58aca84f0fcb52ea63f0eb08de1067b8d406574a715d5e7928f4b67f113a00fb5c5918e74d2327311946c446b242c20fe7347482999bdc1e229b94e27d96',
 }));
 
-vi.mock('../../utils/deviceAuthenticationWrapper.ts', () => ({
+vi.mock('@trezor/device-authenticity', () => ({
     verifyAuthenticityProof: vi.fn().mockResolvedValue({
         valid: true,
         caPubKey: 'test-ca-pubkey',
         rootPubKey: T2B1rootPubKeyOptiga,
     }),
-    getDeviceAuthenticityBlacklistConfig: vi.fn().mockResolvedValue({
+    deviceAuthenticityBlacklistConfig: vi.fn().mockResolvedValue({
         version: 1,
         blacklistedCaPubKeys: [],
         debug: {
             blacklistedCaPubKeys: [],
         },
     }),
-    getDeviceAuthenticityConfig: vi.fn().mockResolvedValue({
+    deviceAuthenticityConfig: vi.fn().mockResolvedValue({
         version: 1,
         T2B1: { rootPubKeysOptiga: [T2B1rootPubKeyOptiga] },
         T3B1: { rootPubKeysOptiga: [] },
