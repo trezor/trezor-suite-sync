186d8a7e0a5b42837cbad6b709e3fbd06044799d
Matěj Husák <matej.husak@icloud.com>
2026-02-12 16:29:24 +0100

    fix: update getOrThrowTest

--- src/getOrThrowTest.ts
+++ src/getOrThrowTest.ts
@@ -4,8 +4,10 @@ export const getOrThrowTest = <T, E>(result: Result<T, E>): T => {
     try {
         return getOrThrow(result);
     } catch (e) {
-        const { cause } = e as Error;
+        if (e instanceof Error) {
+            throw new Error(`getOrThrow: ${e.message}`, { cause: e });
+        }
 
-        throw new Error(`getOrThrow: ${JSON.stringify(cause, null, 2)}`, { cause });
+        throw new Error(`getOrThrow: ${String(e)}`, { cause: e });
     }
 };
