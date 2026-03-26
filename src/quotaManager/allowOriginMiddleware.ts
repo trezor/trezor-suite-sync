0c4b390e569443093229ce7d8688b2654abf6b55
Mattew Husak <matej.husak@icloud.com>
2026-03-26 16:17:09 +0100

    fix(cors): added support for OPTIONS endpoint call

--- src/quotaManager/allowOriginMiddleware.ts
+++ src/quotaManager/allowOriginMiddleware.ts
@@ -4,11 +4,19 @@ import type { FastifyReply, FastifyRequest } from 'fastify';
  *  We generally allow access to Quota Manager from any suite -> which can be also self-hosted by our users, so thus we allow any origin.
  */
 export const allowOriginMiddleware = (
-    _request: FastifyRequest,
+    request: FastifyRequest,
     reply: FastifyReply,
     done: () => void,
 ) => {
     reply.header('access-control-allow-origin', '*');
+    reply.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    reply.header('access-control-allow-headers', '*');
+
+    if (request.method === 'OPTIONS') {
+        reply.status(204).send();
+
+        return;
+    }
 
     done();
 };
